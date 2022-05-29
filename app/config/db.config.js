c99b4b4dce40b603fbfa893a2c2802eac88606e3
Otavio <otaviorodriguesv@hotmail.com>
2022-05-29 16:18:08 -0300

    .env edit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,7 @@
 ﻿
+user = process.env.MONGO_USER
+pass = process.env.MONGO_PASS
 module.exports = {
-    url: "mongodb://127.0.0.1:27017/maistocadasufmg" //process.env.DATABASE_URL
+    //url: "mongodb://127.0.0.1:27017/maistocadasufmg" //process.env.DATABASE_URL
+    url: `mongodb://${user}:${pass}@mongotavio-shard-00-00.tg6dh.mongodb.net:27017,mongotavio-shard-00-01.tg6dh.mongodb.net:27017,mongotavio-shard-00-02.tg6dh.mongodb.net:27017/maistocadasufmg?ssl=true&replicaSet=atlas-x9guje-shard-0&authSource=admin&retryWrites=true&w=majority`
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+require('dotenv').config()
 var corsOptions = {
     origin: "http://localhost:8081"
 };
