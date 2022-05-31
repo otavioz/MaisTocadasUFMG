980e3b9ab3db09ee8991803bf105be288b36f129
Otavio <otaviorodriguesv@hotmail.com>
2022-05-31 19:54:38 -0300

    build with front-end app

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 ﻿const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = __dirname + '/app/views/';
 const app = express();
 require('dotenv').config()
 var corsOptions = {
     origin: "http://localhost:8081"
 };
+
+app.use(express.static(path));
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(bodyParser.json({ limit: '50mb' }));
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-// simple route
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to bezkoder application." });
-});
 
-require("./app/routes/songdata.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
+// simple route
+require("./app/routes/songdata.routes")(app);
+app.get('/*', function (req, res) {
+    res.sendFile(path + "index.html");
+});
 
 //CONNECT
 const db = require("./app/models");
