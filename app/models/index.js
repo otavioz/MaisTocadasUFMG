6a5cbb44ab3365afa5b42661736d316e45d57480
Otavio <otaviorodriguesv@hotmail.com>
2022-07-03 21:12:19 -0300

    ajuste rota incorreta tentativa 3

--- app/models/index.js
+++ app/models/index.js
@@ -5,4 +5,5 @@ const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.songdata = require("./songdata.model.js")(mongoose);
+db.songtuple = require("./songtuple.model.js")(mongoose);
 module.exports = db;
